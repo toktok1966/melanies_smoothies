fe441c2158a819f387722033dbb1a6681bdb77b0
toktok1966 <106530773+toktok1966@users.noreply.github.com>
2025-09-05 21:58:17 +0200

    requsts

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.write(
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
 name_on_order = st.text_input("Name on Smoothi")
 st.write("The Name on the smoothi will be: "+ name_on_order)
