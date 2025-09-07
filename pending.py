8e9b33b909a2170cb146de3b107fc2da46a4c280
toktok1966 <106530773+toktok1966@users.noreply.github.com>
2025-09-07 12:57:00 +0200

    Create pending.py
    initial loading

---
+++ pending.py
@@ -0,0 +1,42 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+from snowflake.snowpark.functions import when_matched
+
+# Write directly to the app
+st.title(f":cup_with_straw: pending smoothie :cup_with_straw:")
+st.write(
+  """Orders that need to be filled.
+  """
+)
+
+# Get the current credentials
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
+#st.dataframe(data=my_dataframe, use_container_width=True)
+
+
+if my_dataframe:
+    editable_df = st.data_editor(my_dataframe)
+    submitted = st.button("Submit")
+    
+    if submitted:
+        
+        og_dataset = session.table("smoothies.public.orders")
+        edited_dataset = session.create_dataframe(editable_df)
+        
+        try:     
+            og_dataset.merge(edited_dataset
+            , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
+            , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+                    )
+            st.success('Orders updated!', icon = '👍')
+        except:
+            st.write('Something went wrong', icon = '👎')
+     #my_update_stmt = """update smoothies.public.orders set order_filled = true
+     #  where name_on_order = 'huhu'"""
+     #st.write(my_update_stmt)
+     #session.sql(my_update_stmt).collect()
+else:    
+    st.success('There is no pending order right now!', icon = '👍')
